2f0a6a12f2578609e05e9e55467e9feb038e4afc
lynyaxacherepaxa <116443590+LYNYAXACHEREPAXA@users.noreply.github.com>
2024-06-15 17:04:00 +0300

    Update win_app.py

--- Logika/m1/u1/win_app.py
+++ Logika/m1/u1/win_app.py
@@ -13,5 +13,18 @@ button = QPushButton('Згенерувати')
 text = QLabel('Натисніть, щоб визначити переможця')
 winner = QLabel('?')
 
+line = QVBoxLayout()
+line.addWidget(text, alignment=Qt.AlignCenter)
+line.addWidget(winner, alignment=Qt.AlignCenter)
+line.addWidget(button, alignment=Qt.AlignCenter)
+main_win.setLayout(line)
+
+def show_winner():
+    number = randint(0, 99)
+    winner.setText(str(number))
+    text.setText('Переможець:')
+
+button.clicked.connect(show_winner)
+
 main_win.show()
 app.exec_()
