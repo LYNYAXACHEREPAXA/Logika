5eb58ddf456959d8d17e26ebbe60d1fbf6c8c1d8
lynyaxacherepaxa <116443590+LYNYAXACHEREPAXA@users.noreply.github.com>
2024-07-20 17:22:48 +0300

    Update menu_window.py

--- menu_window.py
+++ menu_window.py
@@ -1,37 +1,48 @@
-from PyQt5.QtCore import Qt   # основні модулі підключаєм
-from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt      
+from PyQt5.QtWidgets import *  
 
+app = QApplication([])
 
-app = QApplication([]) 
+menu_win = QWidget()
+menu_win.resize(400, 300)
+menu_win.setWindowTitle("Меню")
 
-menu_win = QWidget() 
-menu_win.resize(400,300) # змінюємо розмір віджета
-
-txt_Question = QLineEdit('') #поля для ВВЕДЕННЯ запитання (вписуватиме користувач)
+txt_Question = QLineEdit('')
 txt_Answer = QLineEdit('')
 txt_Wrong1 = QLineEdit('')
 txt_Wrong2 = QLineEdit('')
 txt_Wrong3 = QLineEdit('')
 
-layout_form = QFormLayout() #створюємо форму куди буде вводити користувач
+layout_form = QFormLayout()
 
-layout_form.addRow('Питання', txt_Question) # тексти до поля (вони будуть зліва)
+layout_form.addRow('Питання:', txt_Question)
 layout_form.addRow('Правильна відповідь:', txt_Answer)
-layout_form.addRow('НЕправильна відповідь 1:', txt_Wrong1)
-layout_form.addRow('НЕправильна відповідь 2:', txt_Wrong2)
-layout_form.addRow('НЕправильна відповідь 3:', txt_Wrong3)
+layout_form.addRow('Неправильний варіант №1:', txt_Wrong1)
+layout_form.addRow('Неправильний варіант №2', txt_Wrong2)
+layout_form.addRow('Неправильний варіант №3:', txt_Wrong3)
 
-btn_back = QPushButton('Назад') #кнопки на які можна натискати
+btn_back = QPushButton('Назад')
 btn_add_q = QPushButton('Додати питання')
 btn_clear = QPushButton('Очистити')
 
-hbtn = QHBoxLayout() # горизонтальна лінія на якій будуть розміщені інші
-hbtn.addWidget(btn_back) # додаємо кнопки на лінію
+# Add statistics label
+lbl_statistics = QLabel('')
+
+hbtn = QHBoxLayout()         
+hbtn.addWidget(btn_back)
 hbtn.addWidget(btn_add_q)
 hbtn.addWidget(btn_clear)
 
-vline = QVBoxLayout() 
-vline.addLayout(layout_form) # форму накладаєм на вертикальну лінію
-vline.addLayout(hbtn)  # з'єднуємо горизонтальну та вертикальну лінії 
+# Статистика
+lbl_statistics = QLabel('Статистика:\nПравильних відповідей: 0\nЗагальна кількість спроб: 0') # надпис статистики
 
-menu_win.setLayout(vline) 
+# Макет для основного вікна
+vline = QVBoxLayout()
+vline.addLayout(layout_form)
+vline.addLayout(hbtn)
+vline.addWidget(lbl_statistics) # додаємо його на макет
+
+menu_win.setLayout(vline)
+
+menu_win.setLayout(vline)
+
